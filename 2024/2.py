962b06ad3d36d68aaa6118dff251f9107181385d
benamiller <mrbenamiller@gmail.com>
2024-12-02 08:30:05 +0000

    Solves part 1 with test file

---
+++ 2024/2.py
@@ -0,0 +1,30 @@
+numSafe = 0
+
+with open('2test.txt', 'r') as file:
+
+    lines = file.readlines()
+
+    for line in lines:
+
+        nums = line.split()
+
+        valid = True
+        ascending = int(nums[0]) < int(nums[1])
+
+        for i in range(1, len(nums)):
+            num = int(nums[i])
+            prevNum = int(nums[i - 1])
+
+            if abs(prevNum - num) > 3 or prevNum == num or (ascending and prevNum > num) or (not ascending and num > prevNum):
+                valid = False 
+
+        if valid:
+            numSafe += 1
+
+    print(numSafe)
+
+
+
+
+
+
