71a85a334c142c27f112a35ccad46d297b584f92
benamiller <mrbenamiller@gmail.com>
2024-12-03 10:25:28 +0000

    More pythonic needed

--- 2024/3.py
+++ 2024/3.py
@@ -5,14 +5,14 @@ def sum_muls(text):
 
     matches = re.finditer(pattern, text)
 
-    lineSum = 0
+    line_sum = 0
 
     for match in matches:
         x = int(match.group(1))
         y = int(match.group(2))
-        lineSum += x * y
+        line_sum += x * y
 
-    return lineSum
+    return line_sum
 
 def main():
     sum = 0
@@ -26,3 +26,9 @@ def main():
 if __name__ == "__main__":
     main()
 
+
+# Part 2 - Only aggregate after seeing a do() and not after seeing a don't()
+
+def get_ops(text):
+    mul_pattern = r'mul\((\d{1,3}),(\d{1,3})\)'
+    do_pattern = r'
